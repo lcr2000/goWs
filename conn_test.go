9a4caeb2a7f15a58da85f9799e89a35ec0d4bbbd
fangguizhen <fangguizhen@heywoods.cn>
2021-02-25 14:52:46 +0800

    feat:first commit

---
+++ conn_test.go
@@ -0,0 +1,88 @@
+package goWs
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"sync"
+	"testing"
+)
+
+func TestConn(t *testing.T) {
+	http.HandleFunc("/ws", wsHandler)
+	_ = http.ListenAndServe("0.0.0.0:7777", nil)
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn := NewWsConnection("test", empty{})
+	if err := conn.Open(w, r); err != nil {
+		return
+	}
+	for {
+		msg, err := conn.Receive()
+		if err != nil {
+			break
+		}
+		fmt.Println(string(msg.Data))
+		err = conn.Write(&WsMessage{
+			To:          msg.To,
+			MessageType: msg.MessageType,
+			Data:        msg.Data,
+		})
+		if err != nil {
+			break
+		}
+	}
+}
+
+var m sync.RWMutex
+
+var col map[string]*websocket.Conn
+
+func init() {
+	col = make(map[string]*websocket.Conn)
+}
+
+type empty struct{}
+
+func (e empty) Set(id string, wsConn *websocket.Conn) error {
+	m.RLock()
+	defer m.RUnlock()
+	col[id] = wsConn
+	return nil
+}
+
+func (e empty) Get(id string) (wsConn *websocket.Conn, err error) {
+	m.RLock()
+	defer m.RUnlock()
+	conn, ok := col[id]
+	if !ok {
+		err = errors.New("id is not exist")
+		return
+	}
+	return conn, nil
+}
+
+func (e empty) GetGroup(groupName string) (wsConnList []*websocket.Conn, err error) {
+	m.RLock()
+	defer m.RUnlock()
+	for _, conn := range col {
+		wsConnList = append(wsConnList, conn)
+	}
+	return wsConnList, nil
+}
+
+func (e empty) GetAll() (wsConnList []*websocket.Conn, err error) {
+	m.RLock()
+	defer m.RUnlock()
+	for _, conn := range col {
+		wsConnList = append(wsConnList, conn)
+	}
+	return wsConnList, nil
+}
+
+func (e empty) Del(id string) error {
+	delete(col, id)
+	return nil
+}
---
+++ const.go
@@ -0,0 +1,20 @@
+package goWs
+
+import "errors"
+
+var (
+	// websocket连接已关闭
+	ErrWsConnClose = errors.New("websocket connection closed")
+	// websocket唯一连接id不能为空
+	ErrIdEmpty = errors.New("id is null")
+)
+
+// 描述了消息发送的对象
+const (
+	// 发送给所有在线连接
+	ToAll ToType = "All"
+	// 发送给群组在线连接
+	ToGroup ToType = "Group"
+	// 发送给指定连接
+	ToConn ToType = "Conn"
+)
