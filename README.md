6d5367dd7bd704c398bd75a469067ce222228d72
方贵震 <fangguizhen@heywoods.cn>
2021-03-16 15:10:48 +0800

    feat:ws

--- README.md
+++ README.md
@@ -1,2 +1,174 @@
 # goWs
 基于gorilla封装的并发安全的Golang Websocket组件
+
+## 结构体
+
+WsConnection
+```go
+// WsConnection表示维护的一个websocket类型.
+type WsConnection struct {
+	// 隐藏了内部字段
+}
+```
+WsMessage
+```go
+// WsMessage描述了一个消息实体.
+type WsMessage struct {
+	// 消息发送对象
+	To MsgTo
+	// The message types are defined in RFC 6455, section 11.8.
+	MessageType int
+	// 消息内容
+	Data []byte
+}
+
+// ToType定义了消息发送的对象类型.
+type ToType string
+
+// MsgTo定义了消息发送的对象.
+type MsgTo struct {
+	ToType ToType
+	To     string
+}
+```
+通过`WsMessage`类，在内部进行对应消息的转发，如消息的一对一；一对多等。
+1、`WsMessage`中`To`字段是内部自定义的一个类型`MsgTo`，指定了消息发送的对象，
+`MsgTo`包含字段`ToType`以及字段`To`：字段`ToType`即消息发送的对象类型，分别是`ToAll`(发送给所有在线连接)、`ToGroup`(发送给群组在线连接)以及`ToConn`(发送给指定连接)；
+字段`To`即具体对象。
+2、`MessageType`是消息的具体类型；
+3、`Data`是消息内容。
+
+## 接口
+
+```go
+type Conn interface {
+	Close() error
+	Open(w http.ResponseWriter, r *http.Request) error
+	Receive() (msg *WsMessage, err error)
+	Write(msg *WsMessage) (err error)
+}
+```
+接口`Conn`对外暴露了`4`个方法。方法`Close`即`Close`掉`NewWsConnection`构造的连接；方法`Open`会将`HTTP`服务器连接升级到`WebSocket`协议；
+方法`Receive`接收客户端发送过来的消息；方法`Write`将消息发向客户端。
+
+```go
+// Collection描述了客户端维护的websocket在线连接接口的具体类型.
+type Collection interface {
+	// 将连接写入集合
+	Set(id string, wsConn *websocket.Conn) error
+	// 从集合中获取对应id的连接
+	Get(id string) (wsConn *websocket.Conn, err error)
+	// 通过groupName获取对应的连接列表
+	GetGroup(groupName string) (wsConnList []*websocket.Conn, err error)
+	// 获取所有连接
+	GetAll() (wsConnList []*websocket.Conn, err error)
+	// 将id对应的连接从集合中删除
+	Del(id string) error
+}
+```
+接口`Collection`描述了是如何维护一个在线连接。使用前，请先实现此接口。
+
+```go
+// HeartBeater描述了维持心跳所需要的数据
+type HeartBeater interface {
+	// 校验是否客户端Ping请求
+	IsPingMsg(msg []byte) bool
+	// 获取服务端->客户端Pong请求数据
+	GetPongMsg() []byte
+	// 获取心跳间隔有效时间（单位是秒）。如果两次心跳大于这个有效时间，连接将断开
+	GetHeartbeatTime() int
+}
+```
+接口`HeartBeater`描述了维持心跳所需要的数据。使用前，请先实现此接口。
+
+## 方法说明
+
+```go
+func NewWsConnection(collect Collection, heartBeater HeartBeater) *WsConnection
+```
+方法`NewWsConnection`为新建一个`WsConnection`连接。
+入参`collect`为`Collection`接口类的具体实现；入参`heartBeater`为`HeartBeater`接口的具体实现。
+
+```go
+func (conn *WsConnection) Close() error
+func (conn *WsConnection) Open(w http.ResponseWriter, r *http.Request) error
+func (conn *WsConnection) Receive() (msg *WsMessage, err error)
+func (conn *WsConnection) Write(msg *WsMessage) (err error)
+```
+上面4个方法是接口`Conn`对外暴露的`4`个方法。
+
+## 快速开始
+ ```go
+ import (
+ 	"errors"
+ 	"fmt"
+ 	"github.com/gorilla/websocket"
+ 	"net/http"
+ )
+ 
+func QuickStart(w http.ResponseWriter, r *http.Request) {
+	conn := NewWsConnection(collect{}, beat{})
+	if err := conn.Open(w, r); err != nil {
+		return
+	}
+	for {
+		msg, err := conn.Receive()
+		if err != nil {
+			break
+		}
+		fmt.Println(string(msg.Data))
+		err = conn.Write(&WsMessage{
+			To:          msg.To,
+			MessageType: msg.MessageType,
+			Data:        msg.Data,
+		})
+		if err != nil {
+			break
+		}
+	}
+}
+ ```
+
+collect为实现Collection接口的具体类。
+
+ ```go
+type collect struct{}
+
+func (e collect) Set(id string, wsConn *websocket.Conn) error {
+	return nil
+}
+
+func (e collect) Get(id string) (wsConn *websocket.Conn, err error) {
+	return
+}
+
+func (e collect) GetGroup(groupName string) (wsConnList []*websocket.Conn, err error) {
+	return
+}
+
+func (e collect) GetAll() (wsConnList []*websocket.Conn, err error) {
+	return
+}
+
+func (e collect) Del(id string) error {
+	return nil
+}
+```
+
+beat为实现HeartBeater接口的具体类。
+
+ ```go
+type beat struct{}
+
+func (b beat) IsPingMsg(msg []byte) bool {
+	return false
+}
+
+func (b beat) GetPongMsg() []byte {
+	return []byte{}
+}
+
+func (b beat) GetHeartbeatTime() int {
+	return 10
+}
+```
--- conn.go
+++ conn.go
@@ -1,9 +1,11 @@
 package goWs
 
 import (
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type Conn interface {
@@ -13,13 +15,15 @@ type Conn interface {
 	Write(msg *WsMessage) (err error)
 }
 
-// WsConnection表示维护的一个websocket类型
+// WsConnection表示维护的一个websocket类型.
 type WsConnection struct {
 	// id
 	id string
 	// 在线集合接口
 	collect Collection
-	// websocket
+	// 维持心跳接口
+	heartBeater HeartBeater
+	// 底层websocket
 	wsConn *websocket.Conn
 	// 读队列
 	inChan chan *WsMessage
@@ -27,9 +31,15 @@ type WsConnection struct {
 	outChan chan *WsMessage
 	// 关闭通知
 	closeChan chan struct{}
+	// 最近一次心跳时间
+	lastHeartbeatTime time.Time
+	// 保护closeChan只被执行一次
+	mutex sync.Mutex
+	// closeChan 状态
+	isClosed bool
 }
 
-// 描述了一个消息实体
+// WsMessage描述了一个消息实体.
 type WsMessage struct {
 	// 消息发送对象
 	To MsgTo
@@ -39,42 +49,55 @@ type WsMessage struct {
 	Data []byte
 }
 
-// 定义了消息发送的对象类型
+// ToType定义了消息发送的对象类型.
 type ToType string
 
-// 定义了消息发送的对象
+// MsgTo定义了消息发送的对象.
 type MsgTo struct {
 	ToType ToType
 	To     string
 }
 
-// http升级websocket协议的配置. 允许所有CORS跨域请求
+// http升级websocket协议的配置. 允许所有CORS跨域请求.
 var upgrade = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-// 新建一个WsConnection连接. id为唯一连接id; collect为实现Collection接口类
-func NewWsConnection(id string, collect Collection) *WsConnection {
+// NewWsConnection为新建一个WsConnection连接.
+// collect为Collection接口类的具体实现；heartBeater为HeartBeater接口的具体实现.
+func NewWsConnection(collect Collection, heartBeater HeartBeater) *WsConnection {
 	return &WsConnection{
-		id:        id,
-		collect:   collect,
-		wsConn:    nil,
-		inChan:    make(chan *WsMessage, 1024),
-		outChan:   make(chan *WsMessage, 1024),
-		closeChan: make(chan struct{}, 1),
+		id:                uuid.NewString(),
+		collect:           collect,
+		heartBeater:       heartBeater,
+		wsConn:            nil,
+		inChan:            make(chan *WsMessage, 1024),
+		outChan:           make(chan *WsMessage, 1024),
+		closeChan:         make(chan struct{}, 1),
+		lastHeartbeatTime: time.Now(),
 	}
 }
 
-// 确保WsConnection连接中closeChan只会关闭一次
-var once sync.Once
+// GetWsConnId获取WsConnection中唯一连接id
+func (conn *WsConnection) GetWsConnId() string {
+	return conn.id
+}
 
 func (conn *WsConnection) Close() error {
+	return conn.removeAndClose()
+}
+
+// removeAndClose移除维护的连接、关闭wsConn
+func (conn *WsConnection) removeAndClose() error {
 	_ = conn.wsConn.Close()
-	once.Do(func() {
+	conn.mutex.Lock()
+	defer conn.mutex.Unlock()
+	if !conn.isClosed {
 		close(conn.closeChan)
-	})
+		conn.isClosed = true
+	}
 	return conn.collect.Del(conn.id)
 }
 
@@ -86,7 +109,7 @@ func (conn *WsConnection) Open(w http.ResponseWriter, r *http.Request) error {
 	conn.setWsConn(wsSocket)
 	err = conn.collect.Set(conn.id, wsSocket)
 	if err != nil {
-		_ = conn.Close()
+		_ = conn.removeAndClose()
 		return err
 	}
 	go conn.readLoop()
@@ -94,7 +117,7 @@ func (conn *WsConnection) Open(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-// 将升级得到的WebSocket协议写入WsConnection中
+// setWsConn将升级得到的webSocket协议写入WsConnection中.
 func (conn *WsConnection) setWsConn(wsConn *websocket.Conn) {
 	conn.wsConn = wsConn
 }
@@ -117,12 +140,16 @@ func (conn *WsConnection) Write(msg *WsMessage) (err error) {
 	return
 }
 
+// readLoop通过监听消息,向inChan写入WsMessage.
 func (conn *WsConnection) readLoop() {
 	for {
 		msgType, data, err := conn.wsConn.ReadMessage()
 		if err != nil {
-			_ = conn.Close()
-			break
+			_ = conn.removeAndClose()
+			goto EXIT
+		}
+		if conn.isPing(data) {
+			continue
 		}
 		select {
 		case conn.inChan <- &WsMessage{
@@ -130,22 +157,56 @@ func (conn *WsConnection) readLoop() {
 			Data:        data,
 		}:
 		case <-conn.closeChan:
-			break
+			goto EXIT
 		}
 	}
+EXIT:
+	// 确保连接被关闭
+	return
 }
 
+// isHeatBeat校验数据是否为客户端的Ping数据. 如果是，则向客户端写回Pong数据.
+func (conn *WsConnection) isPing(msg []byte) bool {
+	if !conn.heartBeater.IsPingMsg(msg) {
+		return false
+	}
+	conn.keepAlive()
+	_ = conn.Write(&WsMessage{
+		To: MsgTo{
+			ToType: ToConn,
+			To:     conn.GetWsConnId(),
+		},
+		MessageType: websocket.TextMessage,
+		Data:        conn.heartBeater.GetPongMsg(),
+	})
+	return true
+}
+
+// writeLoop通过监听outChan队列,向websocket写入消息.
 func (conn *WsConnection) writeLoop() {
+	timer := time.NewTimer(time.Duration(conn.heartBeater.GetHeartbeatTime()) * time.Second)
+	defer timer.Stop()
 	for {
 		select {
 		case msg := <-conn.outChan:
 			_ = conn.msgDispatch(msg)
+		case <-timer.C:
+			if !conn.isAlive() {
+				_ = conn.removeAndClose()
+				goto EXIT
+			}
+			timer.Reset(time.Duration(conn.heartBeater.GetHeartbeatTime()) * time.Second)
 		case <-conn.closeChan:
-			break
+			goto EXIT
 		}
 	}
+EXIT:
+	// 确保连接被关闭
+	return
 }
 
+// msgDispatch为消息分发方法.
+// 内部通过判断MsgTo消息中ToType类型，向pushAll、pushGroup、pushConn下发消息.
 func (conn *WsConnection) msgDispatch(msg *WsMessage) error {
 	switch msg.To.ToType {
 	case ToAll:
@@ -159,6 +220,7 @@ func (conn *WsConnection) msgDispatch(msg *WsMessage) error {
 	return nil
 }
 
+// pushAll发送给所有在线连接.
 func (conn *WsConnection) pushAll(msg *WsMessage) error {
 	allWsConn, err := conn.collect.GetAll()
 	if err != nil {
@@ -173,6 +235,7 @@ func (conn *WsConnection) pushAll(msg *WsMessage) error {
 	return nil
 }
 
+// pushGroup发送给群组在线连接.
 func (conn *WsConnection) pushGroup(msg *WsMessage) error {
 	allWsConn, err := conn.collect.GetGroup(msg.To.To)
 	if err != nil {
@@ -187,6 +250,7 @@ func (conn *WsConnection) pushGroup(msg *WsMessage) error {
 	return nil
 }
 
+// pushConn发送给指定连接.
 func (conn *WsConnection) pushConn(msg *WsMessage) error {
 	if msg.To.To == "" {
 		return ErrIdEmpty
@@ -202,11 +266,39 @@ func (conn *WsConnection) pushConn(msg *WsMessage) error {
 	return nil
 }
 
-// 客户端维护的websocket在线连接接口
+// keepAlive更新WsConnection心跳
+func (conn *WsConnection) keepAlive() {
+	conn.lastHeartbeatTime = time.Now()
+}
+
+// isAlive判断WsConnection是否太长时间没有心跳
+func (conn *WsConnection) isAlive() bool {
+	if time.Now().Sub(conn.lastHeartbeatTime) > time.Duration(conn.heartBeater.GetHeartbeatTime())*time.Second {
+		return false
+	}
+	return true
+}
+
+// Collection描述了客户端维护的websocket在线连接接口的具体类型.
 type Collection interface {
+	// 将连接写入集合
 	Set(id string, wsConn *websocket.Conn) error
+	// 从集合中获取对应id的连接
 	Get(id string) (wsConn *websocket.Conn, err error)
+	// 通过groupName获取对应的连接列表
 	GetGroup(groupName string) (wsConnList []*websocket.Conn, err error)
+	// 获取所有连接
 	GetAll() (wsConnList []*websocket.Conn, err error)
+	// 将id对应的连接从集合中删除
 	Del(id string) error
+}
+
+// HeartBeater描述了维持心跳所需要的数据
+type HeartBeater interface {
+	// 校验是否客户端Ping请求
+	IsPingMsg(msg []byte) bool
+	// 获取服务端->客户端Pong请求数据
+	GetPongMsg() []byte
+	// 获取心跳间隔有效时间（单位是秒）。如果两次心跳大于这个有效时间，连接将断开
+	GetHeartbeatTime() int
 }
